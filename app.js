3b4cc0ff62d6744d9a8aafe88503dd40419c08fa
Muscalu Darius <muscaludarius4@gmail.com>
2022-01-05 23:01:49 +0200

    Most of the site is finished. Only left to add the wallet payment

---
+++ app.js
@@ -0,0 +1,38 @@
+
+'use strict';
+
+let ethQunatity = document.getElementById('quantity');
+let decreaseQuantity = document.getElementById('minus');
+let increaseQuantity = document.getElementById('plus');
+let ethPrice = document.getElementById('price');
+let price = 0.10;
+let counter = ethQunatity.textContent;
+
+decreaseQuantity.addEventListener('click', () => {
+    if (ethQunatity.textContent > 1) {
+        ethQunatity.textContent = --counter;
+        price -= 0.1;
+        ethPrice.textContent = price.toFixed(2);
+    }
+});
+
+increaseQuantity.addEventListener('click', () => {
+    if (ethQunatity.textContent < 20) {
+        ethQunatity.textContent = ++counter;
+        price += 0.1;
+        ethPrice.textContent = price.toFixed(2);
+    }
+});
+
+// Increase qunatity minted number
+let numberMinted = document.getElementById('minted-number');
+let holdInitialValue = parseInt(numberMinted.textContent, 10);
+//  Add the random nums to the MINTED number 
+function updateNumberMinted() {
+    if(holdInitialValue < 1930) {
+        holdInitialValue += Math.floor(Math.random() * 10);
+        numberMinted.textContent = holdInitialValue;
+    }
+}
+// Start increasing the number of NFT Minted when page loaded
+setInterval(updateNumberMinted, 500);
